2798d3114b4c629b8b86036f2f94c51b2f89ac02
kevinfekri <120045427+kevinfekri@users.noreply.github.com>
2023-07-05 14:03:05 +0200

    Create mario.c

---
+++ mario-less/mario.c
@@ -0,0 +1,38 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int get_height(void);
+void print_grid(int n);
+
+int main(void)
+{
+    int n = get_height();
+    print_grid(n);
+}
+
+int get_height(void)
+{
+    int n;
+    do
+    {
+        n = get_int("Height :\n");
+    }
+    while (n < 1 || n > 8);
+    return n;
+}
+
+void print_grid(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int k = n - 2; k >= i; k--)
+        {
+            printf(" ");
+        }
+        for (int j = 0; j <= i; j++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
